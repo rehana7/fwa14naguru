781bbbad756b1d2cb4b1e91855fa2030bd60c6e6
rehana7 <89412687+rehana7@users.noreply.github.com>
2021-10-27 20:03:33 -0500

    added the bonus

---
+++ routes/compute.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var storeValue;
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  fake_url = "https://fake.com/path" + req.url
+  const url = new URL(fake_url)
+  const search_params = url.searchParams
+  storeValue = Number(search_params.get("x"))
+  // if paramter is not specified  value of Number(search_params.get("x")) is always zero
+  //console.log(Number(search_params.get("x"))+"hello")
+  if (storeValue == 0)
+    storeValue = Math.random()
+  res.write("Computes the values for Math.cos and math.asin function." + "\n")
+  res.write('Math.cos applied to ' + storeValue + " is " + Math.cos(storeValue) + "\n");
+  res.write('Math.asinh applied to ' + storeValue + " is " + Math.asinh(storeValue) + "\n");
+  res.end('Math.asin applied to ' + storeValue + " is " + Math.asin(storeValue));
+});
+
+module.exports = router;
